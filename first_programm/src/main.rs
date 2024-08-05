d15340613a7152307f63c917349d878d2b98c207
KiriTrip <eggor01042010@gmail.com>
2024-08-05 18:01:04 +0300

    Структуры в виде кортежа

--- first_programm/src/main.rs
+++ first_programm/src/main.rs
@@ -1,27 +1,7 @@
 #[derive(Debug)]
-struct Person {
-    name: String,
-    surname: String,
-    age: i32,
-    balance: f64
-}
-
+struct Animal(i32, String, f32);
 fn main() {
-    let age = 34;
-    let balance = 436.7;
+    let object = Animal(3, "Panda".to_string(), 13.2);
 
-    let person1 = Person {
-        name: "Anna".to_string(),
-        surname: "Smith".to_string(),
-        age,
-        balance
-    };
-
-    let person2 = Person {
-        name: "Denny".to_string(),
-        surname: "Pop".to_string(),
-        ..person1
-    };
-
-
+    println!("{}", object.1);
 }
