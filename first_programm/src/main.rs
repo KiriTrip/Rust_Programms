162f763c122fbe037472e115e2e53fcec6424520
KiriTrip <eggor01042010@gmail.com>
2024-08-05 18:16:13 +0300

    Impl for Triangel

--- first_programm/src/main.rs
+++ first_programm/src/main.rs
@@ -1,7 +1,21 @@
-#[derive(Debug)]
-struct Animal(i32, String, f32);
-fn main() {
-    let object = Animal(3, "Panda".to_string(), 13.2);
+struct Triangel {
+    catet1: f64,
+    catet2: f64
+}
 
-    println!("{}", object.1);
+impl Triangel {
+    fn find_hyp(&self) -> f64 {
+        (self.catet1 * self.catet1 + self.catet2 * self.catet2).sqrt()
+    }
+}
+
+fn main() {
+    let tr1 = Triangel {
+        catet1: 10.0,
+        catet2: 8.0
+    };
+
+    let hyp = tr1.find_hyp();
+
+    println!("{}", hyp);
 }
