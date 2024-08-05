19bfc6db6b62f5a5e799bee9be0334f30c847601
KiriTrip <eggor01042010@gmail.com>
2024-08-05 11:49:28 +0300

    test

---
+++ first_programm/target/debug/deps/first_programm-07886876b560de75.d
@@ -0,0 +1,5 @@
+C:\Users\1\Desktop\Rust_Programms\first_programm\target\debug\deps\libfirst_programm-07886876b560de75.rmeta: src/main.rs
+
+C:\Users\1\Desktop\Rust_Programms\first_programm\target\debug\deps\first_programm-07886876b560de75.d: src/main.rs
+
+src/main.rs:
---
+++ first_programm/target/debug/deps/first_programm-0ea6104c5edbb10e.d
@@ -0,0 +1,5 @@
+C:\Users\1\Desktop\Rust_Programms\first_programm\target\debug\deps\libfirst_programm-0ea6104c5edbb10e.rmeta: src/main.rs
+
+C:\Users\1\Desktop\Rust_Programms\first_programm\target\debug\deps\first_programm-0ea6104c5edbb10e.d: src/main.rs
+
+src/main.rs:
