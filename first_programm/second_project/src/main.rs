6f63412d7c9b9ee36cb81c10ef52667b98757262
KiriTrip <eggor01042010@gmail.com>
2024-08-13 16:35:44 +0300

    first

--- first_programm/second_project/src/main.rs
+++ first_programm/second_project/src/main.rs
@@ -1,13 +1,16 @@
-enum Person {
-    Adult,
-    Underage
+enum MathOperations {
+    Add(f64, f64),
+    Sub(f64, f64),
+    Mul(f64, f64),
+    Div(f64, f64)
 }
-
 fn main() {
-    let person = Person::Adult;
+    let mo = MathOperations::Add(18.0, 9.0);
 
-    match person {
-        Person::Adult => println!("Заходи"),
-        Person::Underage => print!("Пошёл нахуй")
+    match mo {
+        MathOperations::Add(a, b) => println!("{}", a + b),
+        MathOperations::Sub(a, b) => println!("{}", a - b),
+        MathOperations::Mul(a, b) => println!("{}", a * b),
+        MathOperations::Div(a, b) => println!("{}", a / b)
     }
 }
